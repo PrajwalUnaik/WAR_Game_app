33a081902905dfa8f9c3065c9d0f5768fe23e1d5
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2023-12-20 14:56:44 +0530

    chnages to app

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -1,24 +1,104 @@
-//
 //  ContentView.swift
 //  War Card Game
 //
 //  Created by Prajwal U on 20/12/23.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playercard = "card7"
+    @State var CPUcard = "card11"
+    
+    @State var playerscore : Int = 0
+    @State var CPUscore :  Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            
+            Image("background-cloth")
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playercard)
+                    Spacer()
+                    Image(CPUcard)
+                    Spacer()
+                }
+                Spacer()
+                
+                
+                Button(action: { deal()},
+                       label: {Image("button")})
+                                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").font(.headline).padding(.bottom, 10.0)
+                        Text(String(playerscore)).font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU").font(.headline).padding(.bottom, 10.0)
+                        Text (String(CPUscore)).font(.largeTitle)
+                    }
+                    Spacer()
+                }.foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func deal() {
+        
+        // randomise the players card and cpu card
+        var palyercardValue = String(Int.random(in: 2...14))
+        playercard = "card" + palyercardValue
+        var CPUcardvalue = String(Int.random(in: 2...14))
+        CPUcard = "card" + CPUcardvalue
+        
+        //update the score
+        if playercard > CPUcard
+        {
+            playerscore = playerscore + 1
+        }
+        else if playercard < CPUcard{
+            CPUscore += 1
+        }
+        else{ CPUscore += 1
+            playerscore += 1
+        }
     }
 }
+
 
 #Preview {
     ContentView()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
