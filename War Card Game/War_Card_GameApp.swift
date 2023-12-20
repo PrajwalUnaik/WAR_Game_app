4b9293be576990c61d76a8115d49c917f88c19f0
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2023-12-20 11:08:13 +0530

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Prajwal U on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
